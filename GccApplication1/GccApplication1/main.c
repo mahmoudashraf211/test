cf0b0cf615844920ab93fc94fb37a8f58316ca8a
mahmoudashraf211 <mahmoudyouaseif21@gmail.com>
2021-02-10 16:20:14 +0200

    new proj

---
+++ GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 2/10/2021 4:19:32 PM
+ * Author : 20111
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
