e6920ce277c066938f60f01edbb66efc9a4aca55
mahmoudashraf211 <mahmoudyouaseif21@gmail.com>
2021-02-10 15:32:32 +0200

    upload atmel

---
+++ fgd/fgd/main.c
@@ -0,0 +1,18 @@
+/*
+ * fgd.c
+ *
+ * Created: 2/10/2021 3:30:49 PM
+ * Author : 20111
+ */ 
+
+#include <avr32/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
